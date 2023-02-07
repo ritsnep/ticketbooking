cf510b7705456bd0a231073369c88f89df01d9bf
Rits Nep <rits.nep@gmail.com>
2023-02-07 23:48:59 +0545

    HomePage updated

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Color(0x00000000),
+      body: ListView(
+        children: [
+          Container(
+            child: Column(children: [
+              Row(
+                children: [
+                  Column(children: const [
+                    Text("Good Morning"),
+                    Text("Book Ticket")
+                  ]),
+                  Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: const DecorationImage(
+                              fit: BoxFit.cover,
+                              image: AssetImage("assets/images/logo.png"))))
+                ],
+              )
+            ]),
+          )
+        ],
+      ),
     );
   }
 }
